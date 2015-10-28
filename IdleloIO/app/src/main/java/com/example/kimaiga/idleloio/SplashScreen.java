cfdd13badaff18f7fac27f99d35f2303471bbbf0
nelsonkimaiga <nelsonkimaiga@aol.com>
2015-10-28 13:02:32 +0300

    Splash Screen Added

---
+++ IdleloIO/app/src/main/java/com/example/kimaiga/idleloio/SplashScreen.java
@@ -0,0 +1,41 @@
+package com.example.kimaiga.idleloio;
+
+/**
+ * Created by Kimaiga on 10/28/2015.
+ */
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashScreen extends MainActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                   Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+                   // Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+
+
+}
